6d8a6682f7cee20083895e0d604326596913489a
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-23 21:33:41 +0300

    yacen-api: Changed version

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(feature = "grpc-api")]
     tonic_build::configure()
         .build_server(false)
-        .compile_protos(&["proto/yacen/api_v2.proto"], &["proto/yacen"])?;
+        .compile_protos(&["proto/yacen/v1_0/api.proto"], &["proto/yacen/v1_0"])?;
     Ok(())
 }
