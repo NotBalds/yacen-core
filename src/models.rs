f2078c64b91ee290f4f58a93178d653ba4fc0fa5
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-23 22:51:29 +0300

    models.rs: Removed Identity

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,9 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    identity: Identity,
-    known_identities: Vec<Identity>,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Identity {
     name: String,
     secret: [u8; 32],
+    known_identities: HashMap<[u8; 32], String>,
 }
