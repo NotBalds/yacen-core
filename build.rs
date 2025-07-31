11d61bc7a41ce393e09fb082c94e454497aa8521
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-31 22:20:12 +0300

    build.rs: Changed version to latest

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(feature = "grpc-api")]
     tonic_build::configure()
         .build_server(false)
-        .compile_protos(&["proto/yacen/v1_1/api.proto"], &["proto/yacen/v1_1"])?;
+        .compile_protos(&["proto/yacen/latest/api.proto"], &["proto/yacen/latest"])?;
     Ok(())
 }
