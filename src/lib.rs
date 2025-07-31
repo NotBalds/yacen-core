07e325ad8e2c65019d99814e53c29ffc034da1bf
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-31 23:10:21 +0300

    lib.rs: Changed API version v2_0 -> v2_1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "grpc-api")]
 pub mod api {
-    pub const VERSION: &str = "v2_0";
-    tonic::include_proto!("yacen_api.v2_0");
+    pub const VERSION: &str = "v2_1";
+    tonic::include_proto!("yacen_api.v2_1");
 }
 
 pub mod impls;
