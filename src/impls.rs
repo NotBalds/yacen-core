399baed3a264e66c2c6df2d80a00fbd0230227c8
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-01 11:48:58 +0300

    idk

--- src/impls.rs
+++ src/impls.rs
@@ -1,6 +1,6 @@
 use super::models::Config;
-
 use bincode::{config::standard, error::DecodeError, serde::*};
+
 impl Config {
     pub fn to_bytes(self) -> Vec<u8> {
         bincode::serde::encode_to_vec(self, bincode::config::standard()).unwrap()
