759942bae0f126346b62eca0578cd639507fb715
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-07 09:07:09 +0300

    Changed from static array to vec

--- src/models.rs
+++ src/models.rs
@@ -6,6 +6,6 @@ pub const KEYPAIR_BYTES_LENGTH: usize = 83;
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
     name: String,
-    secret: [u8; KEYPAIR_BYTES_LENGTH],
+    secret: Vec<u8>,
     known_identities: HashMap<[u8; 32], String>,
 }
