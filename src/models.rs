ead0a687c5c383c88fb7e250df3061e9b036c44d
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-07 08:59:36 +0300

    Fixed Identity model

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+pub const KEYPAIR_BYTES_LENGTH: u8 = 83;
+
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
     name: String,
-    secret: [u8; 32],
+    secret: [u8; KEYPAIR_BYTES_LENGTH],
     known_identities: HashMap<[u8; 32], String>,
 }
