7d13e004140d66b06b1aa1b0e45190406c643c37
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-07 15:38:22 +0300

    Fixed identity struct

--- src/models.rs
+++ src/models.rs
@@ -5,7 +5,7 @@ pub const KEYPAIR_BYTES_LENGTH: usize = 83;
 
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
-    name: String,
-    secret: Vec<u8>,
-    known_identities: HashMap<[u8; 32], String>,
+    pub name: String,
+    pub secret: Vec<u8>,
+    pub known_identities: HashMap<[u8; 32], String>,
 }
