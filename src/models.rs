1f45feb994be2ce5fde3b8eab6049d3d66d703b8
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-08 12:40:08 +0300

    [MOD] models: reimagined models

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,23 @@
+use crate::security::ed25519::ED25519_PUBLIC_KEY_LEN;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
 pub const KEYPAIR_BYTES_LENGTH: usize = 83;
 
+pub type PathToKey = HashMap<PathBuf, [u8; 32]>;
+
+#[derive(Serialize, Deserialize)]
+pub struct Profile {
+    pub name: String,
+    pub keypair: Vec<u8>,
+    pub ptk: PathToKey,
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
+    pub local_id: String, // hex([32]bytes)
     pub name: String,
-    pub secret: Vec<u8>,
-    pub known_identities: HashMap<[u8; 32], String>,
+    pub public_key: [u8; ED25519_PUBLIC_KEY_LEN],
 }
+
+pub type KnownIdentities = Vec<Identity>;
