31977ae3d94c9c248d0901abd23a62df10fffb52
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-08 12:47:03 +0300

    [MOD] impls: added impl for Profile model

--- src/impls.rs
+++ src/impls.rs
@@ -1,7 +1,17 @@
-use super::models::Identity;
+use super::models::*;
 use bincode::{config::standard, error::DecodeError, serde::*};
 
 impl Identity {
+    pub fn to_bytes(self) -> Vec<u8> {
+        bincode::serde::encode_to_vec(self, bincode::config::standard()).unwrap()
+    }
+
+    pub fn from_bytes(data: &[u8]) -> Result<(Self, usize), DecodeError> {
+        decode_from_slice(data, standard())
+    }
+}
+
+impl Profile {
     pub fn to_bytes(self) -> Vec<u8> {
         bincode::serde::encode_to_vec(self, bincode::config::standard()).unwrap()
     }
