26b11e6354e8d9e83e14ae1101d446b5712bb379
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-08-08 17:43:22 +0300

    [MOD] models: reimagined models x2

--- src/models.rs
+++ src/models.rs
@@ -3,21 +3,21 @@ use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, path::PathBuf};
 
 pub const KEYPAIR_BYTES_LENGTH: usize = 83;
-
-pub type PathToKey = HashMap<PathBuf, [u8; 32]>;
-
-#[derive(Serialize, Deserialize)]
-pub struct Profile {
-    pub name: String,
-    pub keypair: Vec<u8>,
-    pub ptk: PathToKey,
-}
+pub const LOCAL_KEY_BYTES_LENGTH: usize = 32;
 
 #[derive(Serialize, Deserialize)]
 pub struct Identity {
     pub local_id: String, // hex([32]bytes)
     pub name: String,
     pub public_key: [u8; ED25519_PUBLIC_KEY_LEN],
+    pub local_key: [u8; LOCAL_KEY_BYTES_LENGTH],
 }
 
-pub type KnownIdentities = Vec<Identity>;
+pub type KnownIdentities = HashMap<[u8; ED25519_PUBLIC_KEY_LEN], Identity>;
+
+#[derive(Serialize, Deserialize)]
+pub struct Profile {
+    pub name: String,
+    pub keypair: Vec<u8>,
+    pub known_identities: KnownIdentities,
+}
